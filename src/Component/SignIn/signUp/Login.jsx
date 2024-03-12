4d58e93bcb63944a96ec291da80eb7c64718001d
nagudu <halifashuaibu12@gmail.com>
2024-03-12 17:00:58 +0100

    save

---
+++ src/Component/SignIn/signUp/Login.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Button, Card, CardBody, Col, Input, Label, Row } from "reactstrap";
+
+export default function Login() {
+  const [form, setForm] = useState({
+    full_name: "",
+    phone_no: "",
+    email_address: "",
+    password: "",
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setForm((p) => ({ ...p, [name]: value }));
+    console.log(form);
+  };
+  return (
+    <div style={{ marginTop: 80 }}>
+      <Row>
+        <Col md={4}></Col>
+        <Col md={4}>
+          <Card>
+            <Col pt={4} pb={3} px={3}>
+              <Col component="form" role="form">
+                <Col mb={12}>
+                  <Label>Email</Label>
+                  <Input
+                    type="email"
+                    label="Email"
+                    variant="standard"
+                    fullWidth
+                    name="email"
+                    value={form.email}
+                    onChange={handleChange}
+                  />
+                </Col>
+                <Col mb={12}>
+                  <Label>Password</Label>
+                  <Input
+                    type="password"
+                    label="Password"
+                    variant="standard"
+                    fullWidth
+                    name="password"
+                    value={form.password}
+                    onChange={handleChange}
+                  />
+                </Col>
+
+                <Col mt={4} mb={1}>
+                  <Button
+                    style={{ marginTop: 15 }}
+                    variant="gradient"
+                    color="info"
+                    fullWidth
+                    // onClick={handleAdd}
+                  >
+                    sign In
+                  </Button>
+                </Col>
+              </Col>
+            </Col>
+          </Card>
+        </Col>
+        <Col md={4}></Col>
+      </Row>
+    </div>
+  );
+}
--- src/Routes/AppNavigation.jsx
+++ src/Routes/AppNavigation.jsx
@@ -1,33 +1,39 @@
-import React from 'react'
-import { useRoutes } from 'react-router-dom'
-import AppIndex from './AppIndex'
-import Registration from '../Component/Registration'
-import RegistrationTable from '../Component/RegistrationTable'
-import Register from '../Component/Register'
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import AppIndex from "./AppIndex";
+import Registration from "../Component/Registration";
+import RegistrationTable from "../Component/RegistrationTable";
+import Register from "../Component/Register";
+import SignUp from "../Component/SignIn/signUp/Login";
+import Login from "../Component/SignIn/signUp/Login";
 
 export default function AppNavigation() {
   let Pages = useRoutes([
+    {
+      path: "/login",
+      element: <Login />,
+    },
     // {
-    //   path: '/sign-in-form',
+    //   path: '/login',
     //   element: <Register />
-    // }
-       {
-        element: <AppIndex />,
-        children: [
-          {
-            path: '/registration',
-            element: <RegistrationTable />
-          },
-          {
-            path: '/registration-form',
-            element: <Registration />
-          },
-          {
-            path: '/superagent',
-            element: <Registration />
-          },
-        ]
-       }
-    ])
-    return Pages
+    // },
+    {
+      element: <AppIndex />,
+      children: [
+        {
+          path: "/",
+          element: <RegistrationTable />,
+        },
+        {
+          path: "/registration",
+          element: <Registration />,
+        },
+        {
+          path: "/superagent",
+          element: <Registration />,
+        },
+      ],
+    },
+  ]);
+  return Pages;
 }

419e9cfa698a2bec25ef8ce29fbde140c5ac1fb1
ibagwa9 <ibagwai9@gmail.com>
2024-03-15 16:38:24 +0100

    changes

--- src/Component/Vendor/VendorReg.jsx
+++ src/Component/Vendor/VendorReg.jsx
@@ -5,119 +5,119 @@ import { Button, Card, Col, Row, Table, Label, Input } from "reactstrap";
 import { _get } from "../../Utils/Helper";
 
 function VendorReg() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [data, setData] = useState([]);
-  const [filter, setFilter] = useState("");
-  const getReg = useCallback(() => {
-    _get(`vendors?query_type=select-all&plate_no=${filter}`, (resp) => {
-      if (resp.success && resp.results) {
-        setData(resp.results);
-      }
-    });
-  }, [filter]);
+    const [data, setData] = useState([]);
+    const [filter, setFilter] = useState("");
+    const getReg = useCallback(() => {
+        _get(`vendors?query_type=select-all&plate_no=${filter}`, (resp) => {
+            if (resp.success && resp.results) {
+                setData(resp.results);
+            }
+        });
+    }, [filter]);
 
-  useEffect(() => {
-    getReg();
-  }, [getReg]);
-  return (
-    <Card className="app_card dashboard_card shadow p-4 m-2 mt-2">
-      <Row>
-        <Col md={12} style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-          <h4 className="app_title"> Registered Vendors </h4>
-          <button
-            className="app_button"
-            style={{
-              width: 150,
-              padding: 10,
-              marginLeft: 15,
-              color: "#000",
-              borderRadius: 7,
-            }}
-            onClick={() => navigate("/vendor")}
-          >
-            Add vendor +
-          </button>
-        </Col>
-      </Row>
-      <hr style={{ width: "100%"}} />
-      <Row>
-        <Col md={12}>
-          <div style={{ display: "flex", flexDirection: "row" }}>
-            <Col md={12}>
-              <div className="search1">
-                <CiSearch
-                  style={{
-                    fontSize: 30,
-                    width: 25,
-                    marginTop: 3,
-                    color: "#000",
-                  }}
-                />
-                <Input
-                  style={{
+    useEffect(() => {
+        getReg();
+    }, [getReg]);
+    return (
+        <Card className="app_card dashboard_card shadow p-4 m-2 mt-2">
+            <Row>
+                <Col md={12} style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                    <h4 className="app_title"> Registered Vendors </h4>
+                    <button
+                        className="app_button"
+                        style={{
+                            width: 150,
+                            padding: 10,
+                            marginLeft: 15,
+                            color: "#000",
+                            borderRadius: 7,
+                        }}
+                        onClick={() => navigate("/vendor")}
+                    >
+                        Add vendor +
+                    </button>
+                </Col>
+            </Row>
+            <hr style={{ width: "100%" }} />
+            <Row>
+                <Col md={12}>
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                        <Col md={12}>
+                            <div className="search1">
+                                <CiSearch
+                                    style={{
+                                        fontSize: 30,
+                                        width: 25,
+                                        marginTop: 3,
+                                        color: "#000",
+                                    }}
+                                />
+                                <Input
+                                    style={{
+                                        position: "relative",
+                                        width: "100%",
+                                        fontSize: 20,
+                                        top: -5,
+                                    }}
+                                    name="filter"
+                                    value={filter}
+                                    type="text"
+                                    className="app_input2"
+                                    onChange={({ target: { value } }) => setFilter(value)}
+                                    placeholder="Search: eg. Vendor Name | Vendor ID"
+                                />
+                            </div>
+                        </Col>
+                        <Label
+                            onClick={getReg}
+                            className="label_title1"
+                            style={{ color: "#000", cursor: "pointer" }}
+                        >
+                            Search
+                        </Label>
+                    </div>
+                </Col>
+            </Row>
+            <Table
+                bordered
+                responsive
+                style={{
                     position: "relative",
+                    top: "20px",
                     width: "100%",
-                    fontSize: 20,
-                    top: -5,
-                  }}
-                  name="filter"
-                  value={filter}
-                  type="text"
-                  className="app_input2"
-                  onChange={({ target: { value } }) => setFilter(value)}
-                  placeholder="Search: eg. Vendor Name | Vendor ID"
-                />
-              </div>
-            </Col>
-            <Label
-              onClick={getReg}
-              className="label_title1"
-              style={{ color: "#000", cursor: "pointer" }}
+                    marginTop: "4px",
+                }}
             >
-              Search
-            </Label>
-          </div>
-        </Col>
-      </Row>
-      <Table
-        bordered
-        responsive
-        style={{
-          position: "relative",
-          top: "20px",
-          width: "100%",
-          marginTop: "4px",
-        }}
-      >
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Vendor Name</th>
-            <th>Phone Number</th>
-            <th>Vendor email</th>
-            <th>Office Address</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((vendor, idx) => (
-            <tr key={idx}>
-              <th scope="row">{idx + 1}</th>
-              <td>{vendor.vendor_name}</td>
-              <td>{vendor.vendor_org_phone}</td>
-              <td>{vendor.vendor_org_email}</td>
-              <td>{vendor.vendor_ofiice_address}</td>
-              <td className="text-center">
-                <Button color="info" className="marginResponsive">View</Button>
-                <Button color="success">Top up</Button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </Card>
-  );
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Vendor Name</th>
+                        <th>Phone Number</th>
+                        <th>Vendor email</th>
+                        <th>Office Address</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((vendor, idx) => (
+                        <tr key={idx}>
+                            <th scope="row">{idx + 1}</th>
+                            <td>{vendor.vendor_name}</td>
+                            <td>{vendor.vendor_org_phone}</td>
+                            <td>{vendor.vendor_org_email}</td>
+                            <td>{vendor.vendor_ofiice_address}</td>
+                            <td className="text-center">
+                                <Button color="info" className="marginResponsive">View</Button>
+                                <Button color="success">Top up</Button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </Card>
+    );
 }
 
 export default VendorReg;
--- src/main.jsx
+++ src/main.jsx
@@ -11,12 +11,10 @@ import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-
     </Provider>
   </React.StrictMode>
 );

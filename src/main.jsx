97ed86dc6d2357fcd3d9da2193d3019a2fffaf2f
ibagwa9 <ibagwai9@gmail.com>
2024-03-15 15:52:07 +0100

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+
+    </Provider>
   </React.StrictMode>
 );

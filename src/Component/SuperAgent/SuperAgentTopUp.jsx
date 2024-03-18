0e7b07b2980560278e5050a77d468d2de4abdbf3
Nazif Abdullahi <nazifabdullahi003@gmail.com>
2024-03-18 11:26:36 +0100

    save

--- src/Component/SuperAgent/SuperAgentTopUp.jsx
+++ src/Component/SuperAgent/SuperAgentTopUp.jsx
@@ -19,102 +19,25 @@ function SuperAgentTopUp() {
   };
 
   return (
-    <div className="app_card dashboard_card m-0 p-0">
-      <span
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          fontWeight: "600",
-          fontSize: "18px",
-        }}
-      >
-        Super Agent Top-Up
-      </span>
+    <>
+      <div className="app_card dashboard_card m-0 p-0">
+        <h3 className="text-center fw-bold">Super Agent Top-Up</h3>
 
-
-      <div
-        style={{
-          position: "relative",
-          paddingLeft: '52px',
-          top: "30px",
-          margin: "0 auto",
-          width: "40%",
-          height: "65vh",
-          borderRadius: "8px",
-          border: "1px solid #f5c005",
-          boxShadow: '1px 2px #f5c005'
-        }}
-      >
-      <div>
         <div
           style={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "15px",
-            marginBottom: '20px'
+            margin: "0 auto",
+            width: "60%",
+            height: "60vh",
+            borderRadius: "5px",
+            border: "1px solid #f5c005",
+            padding: "20px",
           }}
         >
-          <span
-            style={{
-                position: 'relative',
-                left: '-56px',
-              display: "flex",
-              alignItems: "center",
-              fontWeight: "600",
-              fontSize: "15px",
-            }}
-          >
-            Super Agent:
-          </span>
-          <VendorDropdown />
-          {/* <Select
-            value={selectedVendor}
-            onChange={handleSelectChange}
-            options={data}
-            placeholder="Search for a super agent ........"
-            styles={{
-              borderRadius: "none !important",
-              border: "1px solid #f5c005 !important",
-              marginBottom: "15px",
-              width: "50%",
-              padding: "8px",
-            }}
-            isLoading={loading}
-          /> */}
-        </div>
-        <p style={{fontSize: '15px', fontWeight: '600', marginBottom: '20px'}}>Name : <span style={{marginLeft: '72px', fontWeight: '400'}}>Ibrahim Auwal</span></p>
-        <p style={{fontSize: '15px', fontWeight: '600', marginBottom: '20px'}}>ID : <span style={{marginLeft: '95px', fontWeight: '400'}}>12345</span></p>       
-        </div>
-       
-        <div
-          style={{
-            position: "relative",
-            top: "20px",
-          }}>
-      {/* <div className="app_card dashboard_card m-0 p-0"> */}
-        {/* <h3 className="text-center fw-bold">Super Agent Top-Up</h3> */}
-
-        <div
-          // style={{
-          //   margin: "0 auto",
-          //   width: "60%",
-          //   height: "60vh",
-          //   borderRadius: "5px",
-          //   border: "1px solid #f5c005",
-          //   padding: "20px",
-          // }}
-        >
-          {JSON.stringify(form)}
+          {/* {JSON.stringify(form)} */}
           <div className="agent">
             <div
               style={{
-                position: 'relative',
-                left: '-35px',
                 display: "flex",
-                alignItems: "center",
-                marginRight: "38px",
-                fontWeight: "600",
-                fontSize: "15px",
                 marginTop: "15px",
               }}
             >
@@ -124,71 +47,10 @@ function SuperAgentTopUp() {
                 selectedAgentValue={form.vendor}
               />
             </div>
-            {/* <h3>Name : {form.vendor_name}</h3>
-            <h3>ID : {form.vendor_id}</h3> */}
+            <h3>Name : {form.vendor_name}</h3>
+            <h3>ID : {form.vendor_id}</h3>
           </div>
-          <p style={{fontSize: '15px', fontWeight: '600', marginRight: '57px', marginBottom: '20px', marginTop: '20px'}}>Name : <span  style={{marginLeft:  '72px', fontWeight: '400'}}>qwerty</span></p>
-          <p style={{fontSize: '15px', fontWeight: '600', marginRight: '57px', marginBottom: '20px'}}>ID :  <span  style={{marginLeft: '95px', fontWeight: '400'}}>1233344455</span></p>
-        </div>
-
-
-        <span
-          style={{
-            position: "relative",
-            top: "2.5rem",
-            display: "flex",
-            alignItems: "center",
-            marginRight: "10px",
-            fontWeight: "600",
-            fontSize: "15px",
-          }}
-        >
-          Balance: 
-          <span  style={{marginLeft: '60px', fontWeight: '400'}}>1233344455</span>
-        </span>
-        <div>
-          <span
-            style={{
-              position: "relative",
-              top: "4rem",
-              display: "flex",
-              alignItems: "center",
-              marginRight: "10px",
-              fontWeight: "600",
-              fontSize: "15px",
-            }}
-          >
-            Amount
-          </span>
-          <input
-            placeholder="Enter amount here..."
-            // className="app_input-topUp"
-            style={{
-              position: 'relative',
-              width: '50%',
-              left: '12.7rem',
-              top: '1.7rem',
-              height: '32px',
-              paddingLeft: '5px',
-              borderRadius: '5px',
-              border: '1px solid',
-            }}
-          />
-        </div>
-        <button
-          style={{
-            position: 'relative',
-                top: '30px',
-                borderRadius: '5px',
-                border: '0px solid',
-                height: '40px',
-                backgroundColor: '#f5c005',
-                left: '13rem',
-                width: '150px',
-                boxShadow: '1px 2px gray'
-          }}
-        >Submit</button>
-          {/* <div className="vehicle">
+          <div className="vehicle">
             <div
               style={{
                 display: "flex",
@@ -201,8 +63,8 @@ function SuperAgentTopUp() {
                 selectedSuperValue={form.super_id}
               />
             </div>
-            <h3>Name : {form.name}</h3>
-            <h3>ID : {form.id}</h3>
+            <h3>Name : {form.super_id}</h3>
+            <h3>ID : {form.super_name}</h3>
 
             <div style={{ display: "flex" }}>
               <h3>Amount:</h3>
@@ -215,20 +77,19 @@ function SuperAgentTopUp() {
                 type="number"
               />
             </div>
-          </div> */}
+          </div>
 
-          {/* <div
+          <div
             style={{
               marginTop: "auto",
             }}
           >
             <Button onClick={submitTopUp}>Submit</Button>
-          </div> */}
+          </div>
         </div>
       </div>
-      </div>
-      // </div>
-      );
+    </>
+  );
 }
 
 export default SuperAgentTopUp;

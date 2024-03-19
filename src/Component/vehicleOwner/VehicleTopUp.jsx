a1aab9edbf5734fb5dad0c9354deda7b2afd8f1c
ibagwa9 <ibagwai9@gmail.com>
2024-03-19 15:47:03 +0100

    changes

--- src/Component/vehicleOwner/VehicleTopUp.jsx
+++ src/Component/vehicleOwner/VehicleTopUp.jsx
@@ -38,81 +38,82 @@ function VehicleTopUp({ selectedAgentValue, selectedVehicleValue }) {
     e.preventDefault();
     setLoading(true)
     _post(`top-up/create`,
-    form,
-    (res)=> {
-      if(res.success){
-        toast.success(`Sucessfully added ${form.amount}`)
-        navigate('/vehicleownertable')
+      form,
+      (res) => {
+        if (res.success) {
+          toast.success(`Sucessfully added ${form.amount}`)
+          navigate('/vehicleownertable')
+        }
+      },
+      err => {
+        console.log(err)
+        setLoading(false)
       }
-    },
-    err =>{
-      console.log(err)
-      setLoading(false)
-    } 
     )
     //setLoading(false)
     //console.log(form);
   };
 
-  return (
-    <>
-      <div className="app_card dashboard_card m-0 p-0">
+  return (<>
+    <div className="app_card dashboard_card m-0 p-0">
+      <div className='middle-card-topup' style={{ margin: "auto" }}>
         <h3 className="text-center fw-bold ve-t-u" >Vehicle Top-Up</h3>
 
-          {/* {JSON.stringify(form)} */}
-          <div className="agent">
-            <div className='account-info row'>
-              <div className='info-input col-md-6'>
-                <h4 style={{ marginRight: '20px'}}> Select Agent:</h4>
-                 <AgentDropDown
-                  handleChange={handleChange}
-                 selectedAgentValue={form.agent_id}
-                />
-              </div>
-              
-             <div className='info-input col-md-6' >
-              <h4 style={{ marginRight: '57px'}}> Vehicle:</h4>
+        {/* {JSON.stringify(form)} */}
+        <div className="agent app_card " style={{ padding: "50px" }}>
+          <div className='account-info row'>
+            <div className='info-input col-md-6'>
+              <h4 style={{ marginRight: '20px' }}> Select Agent:</h4>
+              <AgentDropDown
+                handleChange={handleChange}
+                selectedAgentValue={form.agent_id}
+              />
+            </div>
+
+            <div className='info-input col-md-6' >
+              <h4 style={{ marginRight: '57px' }}> Vehicle:</h4>
               <VehicleDropDown
                 handleChange={handleChange}
                 selectedVehicleValue={form.vehicle_id}
               />
             </div>
-             <div className='info-input col-md-6 mx-auto mb-5'>
+            <div className='info-input col-md-6 mx-auto mb-5'>
               <h4>Amount:</h4>
               <input
-                 className="form-control"
+                className="form-control"
                 placeholder="Enter amount here..."
                 onChange={handleChange}
                 name="amount"
                 value={form.amount}
                 type="number"
-               
+
               />
             </div>
           </div>
 
           <div
             style={{
-              marginTop: "auto",
+              marginTop: "50px",
             }}
           >
-            <Button 
-            onClick={submitTopUp}
+            <Button
+              onClick={submitTopUp}
               disabled={loading}
-            style={{
+              style={{
                 marginInline: 'auto',
-                display:'block',
+                display: 'block',
                 height: '40px',
                 backgroundColor: '#f5c005',
                 left: '23rem',
                 width: '28rem',
                 boxShadow: '1px 2px gray'
-            }}
+              }}
             >Submit</Button>
           </div>
         </div>
       </div>
-    </>
+    </div>
+  </>
   );
 }
 

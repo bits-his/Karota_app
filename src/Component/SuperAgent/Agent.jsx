46123e06f60aef1f78996d90233dc32ec712696a
Nazif Abdullahi <nazifabdullahi003@gmail.com>
2024-03-22 15:11:32 +0100

    super agent validation

--- src/Component/SuperAgent/Agent.jsx
+++ src/Component/SuperAgent/Agent.jsx
@@ -81,7 +81,7 @@ export default function Agent() {
       newErrors.name = "Name must be filled";
     }
     if (!formData.phone_no.trim()) {
-      newErrors.phone_no = "Phone Number No must be filled";
+      newErrors.phone_no = "Phone Number must be filled";
     }
     if (!formData.email.trim()) {
       newErrors.email = "Email must be filled";

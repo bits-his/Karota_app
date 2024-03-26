9149075558ec25beba9406d41d56d23a693d51f5
Phisherman <117809934+Phisherman-0@users.noreply.github.com>
2024-03-26 17:09:13 +0100

    save changes on view

--- src/Component/Vendor/VendorDetail.jsx
+++ src/Component/Vendor/VendorDetail.jsx
@@ -3,14 +3,28 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Table, Card, Row, Col, Button } from "reactstrap";
 import keke from "../../assets/keke_napep.png";
-import { _get, _post } from "../../Utils/Helper";
+import { _get, _post, separator } from "../../Utils/Helper";
 
 export default function VendorDetail() {
   const navigate = useNavigate();
   const { user } = useSelector((s) => s.auth);
   const [data, setData] = useState([]);
+  const [details, setDetails] = useState({})
   const params = useParams();
   const owner_id = params.id;
+
+  const getData = useCallback(() => {
+    _get(`vendors?query_type=select-all&id=${owner_id}`, (resp) => {
+      if (resp.success && resp.results) {
+        setDetails(resp.results[0]);
+      }
+    });
+  }, [owner_id]);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
   const getReg = useCallback(() => {
     _post(
       `top-up/create`,
@@ -75,24 +89,24 @@ export default function VendorDetail() {
         <Col md={12}>
           <section style={{ marginBottom: "2rem" }}>
             <div style={{ display: "flex" }}>
-              <div style={{ width: "50%", marginBottom: "20px" }}>
-                <p>Vendor's name: </p>
-                <span>{data.vendor_name}</span>
+              <div style={{ width: "50%", marginBottom: "20px", display: "flex" }}>
+                <p style={{ marginRight: 10, fontSize: 16, fontWeight: "bold" }}>Vendor's name: </p>
+                <span>{details.vendor_name}</span>
               </div>
-              <div style={{ width: "50%" }}>
-                <p>Phone no.: </p>
-                <span>{data.vendor_org_phone}</span>
+              <div style={{ width: "50%", display: "flex" }}>
+                <p style={{ marginRight: 10, fontSize: 16, fontWeight: "bold" }}>Phone no.: </p>
+                <span>{details.vendor_org_phone}</span>
               </div>
             </div>
 
             <div style={{ display: "flex" }}>
-              <div style={{ width: "50%" }}>
-                <p>Address: </p>
-                <span>{data.vendor_ofiice_address}</span>
+              <div style={{ width: "50%", display: "flex" }}>
+                <p style={{ marginRight: 10, fontSize: 16, fontWeight: "bold" }}>Address: </p>
+                <span>{details.vendor_ofiice_address}</span>
               </div>
-              <div style={{ width: "50%" }}>
-                <p>E-mail: </p>
-                <span>{data.vendor_org_email}</span>
+              <div style={{ width: "50%", display: "flex" }}>
+                <p style={{ marginRight: 10, fontSize: 16, fontWeight: "bold" }}>E-mail: </p>
+                <span>{details.vendor_org_email}</span>
               </div>
             </div>
           </section>
@@ -100,11 +114,11 @@ export default function VendorDetail() {
           <Table striped bordered>
             <thead>
               <tr className="table-dark">
-                <th scope="row">Date</th>
-                <th scope="row">Type</th>
-                <th scope="row">Description</th>
-                <th scope="row">Amount</th>
-                <th scope="row">Balance</th>
+                <th scope="row" className="text-center">Date</th>
+                <th scope="row" className="text-center">Type</th>
+                <th scope="row" className="text-center">Description</th>
+                <th scope="row" className="text-center">Amount</th>
+                <th scope="row" className="text-center">Balance</th>
               </tr>
             </thead>
             <tbody>
@@ -113,8 +127,8 @@ export default function VendorDetail() {
                   <td>{item.t_date}</td>
                   <td>{item.type_of_top_up}</td>
                   <td>{item.description}</td>
-                  <td>{item.credit}</td>
-                  <td>{item.balance}</td>
+                  <td className="text-right">{separator(item.credit)}</td>
+                  <td className="text-right">{separator(item.balance)}</td>
                 </tr>
               ))}
             </tbody>

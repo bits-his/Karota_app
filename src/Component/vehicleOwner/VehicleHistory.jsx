5088861e4cf56388eaf3d2a36c277d8fc8621c9e
Ichie Ozor <simeon_mc2000@yahoo.com>
2024-03-27 14:18:25 +0100

    update

--- src/Component/vehicleOwner/VehicleHistory.jsx
+++ src/Component/vehicleOwner/VehicleHistory.jsx
@@ -1,18 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useParams } from 'react-router'
-import { _get } from '../../Utils/Helper'
+import { _get, _post } from '../../Utils/Helper'
 
 export default function VehicleHistory() {
-    const vehicle_id = useParams()
+    const params = useParams()
+    const vehicle_id = params.id
+    console.log(vehicle_id)
     const [transactions, setTransactions] = useState([])
+
+
+
+    const getReg = useCallback(() => {
+      _post(
+        `top-up/create`,
+        {
+          source_id: vehicle_id,
+          query_type: "select_destination",
+        },
+        (resp) => {
+          if(resp.success && resp.result){
+            console.log(resp)
+          }
+        }
+      )
+    }, [vehicle_id])
     useEffect(()=> {
-        _get(`trans_history?query_type=vehicles&vehicle_id${vehicle_id}`,
-        res => {
-          console.log(res.data)
-        },[])
-    })
+        // _get(`trans_history?query_type=vehicles&vehicle_id${vehicle_id}`,
+        // res => {
+        //   console.log(res.data)
+        // },[])
+      getReg()
+    }, [getReg])
   return (
-    <div>VehicleHistory
+    <div>VehicleHistory 3333
       {transactions}
     </div>
   )
